a5bf76b4d77e388c3d7f859fc1769eee348aa484
LuftVerbot <97435834+LuftVerbot@users.noreply.github.com>
2024-05-26 02:05:47 +0200

    Update DownloadReceiver.kt

--- app/src/main/java/dev/brahmkshatriya/echo/download/DownloadReceiver.kt
+++ app/src/main/java/dev/brahmkshatriya/echo/download/DownloadReceiver.kt
@@ -1,6 +1,5 @@
 package dev.brahmkshatriya.echo.download
 
-import android.app.DownloadManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -13,6 +12,7 @@ import com.kyant.taglib.Metadata
 import com.kyant.taglib.Picture
 import com.kyant.taglib.PropertyMap
 import com.kyant.taglib.TagLib
+import com.tonyodev.fetch2.*
 import dagger.hilt.android.AndroidEntryPoint
 import dev.brahmkshatriya.echo.EchoDatabase
 import dev.brahmkshatriya.echo.common.models.Track
@@ -33,52 +33,34 @@ class DownloadReceiver : BroadcastReceiver() {
     lateinit var database: EchoDatabase
 
     private val downloadDao: DownloadDao by lazy { database.downloadDao() }
+
     override fun onReceive(context: Context?, intent: Intent?) {
         context ?: return
         val action = intent?.action ?: return
-        if (DownloadManager.ACTION_DOWNLOAD_COMPLETE == action) {
-            val downloadId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+        if (action == Fetch.ACTION_FETCH_COMPLETE) {
+            val downloadId = intent.getLongExtra(Fetch.EXTRA_DOWNLOAD_ID, -1L).toInt()
             val download = runBlocking {
-                withContext(Dispatchers.IO) { downloadDao.getDownload(downloadId) }
+                withContext(Dispatchers.IO) { downloadDao.getDownload(downloadId.toLong()) }
             } ?: return
-            val track =
-                context.getFromCache(download.itemId, Track.creator, "downloads") ?: return
+            val track = context.getFromCache(download.itemId, Track.creator, "downloads") ?: return
 
-            val downloadManager =
-                context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-            val query = DownloadManager.Query().setFilterById(downloadId)
-            val cursor = downloadManager.query(query)
-            if (cursor.moveToFirst()) {
-                val columnIndex = cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI)
-                val fileUri = cursor.getString(columnIndex)
-                if (fileUri != null) {
-                    val mediaTypeIndex = cursor.getColumnIndex(DownloadManager.COLUMN_MEDIA_TYPE)
-                    val mimeType = cursor.getString(mediaTypeIndex)
-                    val extension = MimeTypeMap.getSingleton().getExtensionFromMimeType(mimeType)
-                    val file = File(Uri.parse(fileUri).path!!)
-                    val newFile = File("${file.absolutePath}.$extension")
-                    file.renameTo(newFile)
-                    context.applyTags(newFile, track)
-                    MediaScannerConnection.scanFile(
-                        context, arrayOf(newFile.toString()), null, null
-                    )
+            val fetch = Fetch.getDefaultInstance()
+            fetch.getDownload(downloadId) { download ->
+                download?.let {
+                    val file = File(it.file)
+                    context.applyTags(file, track)
+                    MediaScannerConnection.scanFile(context, arrayOf(file.toString()), null, null)
+                    runBlocking {
+                        withContext(Dispatchers.IO) { downloadDao.deleteDownload(downloadId.toLong()) }
+                    }
                 }
-            }
-            cursor.close()
-            runBlocking {
-                withContext(Dispatchers.IO) { downloadDao.deleteDownload(downloadId) }
             }
         }
     }
 
     companion object {
-        fun Context.applyTags(
-            newFile: File, track
-            : Track
-        ) {
-            val fd = ParcelFileDescriptor.open(
-                newFile, ParcelFileDescriptor.MODE_READ_WRITE
-            )
+        fun Context.applyTags(newFile: File, track: Track) {
+            val fd = ParcelFileDescriptor.open(newFile, ParcelFileDescriptor.MODE_READ_WRITE)
 
             val metadata = TagLib.getMetadata(fd = fd.dup().detachFd(), readPictures = true)
                 ?: Metadata(PropertyMap(), arrayOf())
