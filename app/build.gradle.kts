654642321368514df5efd700d7676c98b0340c44
LuftVerbot <97435834+LuftVerbot@users.noreply.github.com>
2024-05-26 02:23:20 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,8 +76,8 @@ dependencies {
 
     //TODO : use fetch instead of download manager
 //    implementation("com.github.tonyofrancis.Fetch:xfetch2:3.1.6")
-      implementation("com.tonyodev.fetch2:fetch2:3.0.12")
-      implementation("com.tonyodev.fetch2okhttp:fetch2okhttp:3.0.12")
+      implementation("androidx.tonyodev.fetch2:xfetch2:3.1.6")
+      implementation("androidx.tonyodev.fetch2okhttp:xfetch2okhttp:3.0.12")
       implementation("androidx.legacy:legacy-support-v4:1.0.0")
       implementation("androidx.work:work-runtime-ktx:2.7.1")
 
